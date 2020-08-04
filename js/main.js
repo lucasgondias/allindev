6cb244a189077f2e61af352c1958ec52363e64f3
lucasgondias <lucas.alexandregdias@gmail.com>
2020-08-04 16:56:11 -0300

    location

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+//Obter localização do pc
+var x = document.getElementById("demo");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+
+
+
 (function ($) {
   "use strict";
 
