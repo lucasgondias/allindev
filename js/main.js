0e6bf52659a91ff8066566c3803f06ce6ea9f1b0
lucasgondias <lucas.alexandregdias@gmail.com>
2020-08-05 12:24:30 -0300

    insert location in modal

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 //Obter localização do pc
-// var x = document.getElementById("demo");
-// function getLocation() {
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(showPosition);
-//   } else {
-//     x.innerHTML = "Geolocation is not supported by this browser.";
-//   }
-// }
+ var x = document.getElementById("demo");
+ function getLocation() {
+   if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition(showPosition);
+   } else {
+     x.innerHTML = "Geolocation is not supported by this browser.";
+   }
+ }
 
-// function showPosition(position) {
-//   x.innerHTML = "Latitude: " + position.coords.latitude +
-//   "<br>Longitude: " + position.coords.longitude;
-// };
+function showPosition(position) {
+   x.innerHTML = "Latitude: " + position.coords.latitude +
+   "<br>Longitude: " + position.coords.longitude;
+ };
 
 
 //modal
